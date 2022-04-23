ab4844d2e7de82920ee8e13e4cdb7f8935972863
Lucas Dominguez <pereiraluccas68@gmail.com>
2022-04-23 15:34:53 -0300

    primeiro commit

---
+++ _js/script.js
@@ -0,0 +1,12 @@
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    variableWidth: true,
+    centerMode: true,
+    centerPadding: '190px',
+    autoplay: true,
+    autoplaySpeed: 2000
+});
